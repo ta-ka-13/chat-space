bb13a7bbae8016c99b350c10cf8858b64a9178ae
ta-ka-13 <edll5505d@yahoo.co.jp>
2019-10-29 18:00:51 +0900

    Create groups_controller.rb

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,39 @@
+class GroupsController < ApplicationController
+
+  before_action :set_group, only: %i(edit update)
+  
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if group.save
+      redirect_to group_messages_path(@group), notice:"グループを作成しました"
+    eles
+      flash.now[:alert] = "グループ作戦に失敗しました"
+      render action: :new
+    end    
+  end
+
+  def edit ; end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: "グループ編集に失敗しました"
+      render action: :edit
+    end
+  end
+
+  
+private
+
+ def group_params
+ params.require(:group).permit(:name, { :user_ids: [] })
+ end
+
+ def set_group
+  @group = Group.find(params[:id])
+ end
+
+end
