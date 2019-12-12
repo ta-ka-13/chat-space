80345c8ff5af731e6914b7d8f78527d799698ee2
ta-ka-13 <edll5505d@yahoo.co.jp>
2019-12-12 17:40:26 +0900

    非同期送信　controller,jbuilder,etc

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,51 +1,51 @@
 class MessagesController < ApplicationController
 
-  before_action :set_group
+    before_action :set_group
 
-    def index
-      @message = Message.new
-      @messages = @group.messages.includes(:user)
-      @group = Group.find(params[:group_id])
+      def index
+        @message = Message.new
+        @messages = @group.messages.includes(:user)
+        @group = Group.find(params[:group_id])
+        
+      end
+
+
+      def create
+
+        @message = @group.messages.new(message_params)
+
+      
+
+        if @message.save
+  # binding.pry
+          respond_to do |format|
+            format.html { redirect_to group_messages_path(@group) }
+            format.json
+          end
+
+        else
+          @messages = @group.messages.includes(:user)
+          flash.now[:alert] = 'メッセージを入力してください'
+          render :index
+        end
+
+      end
+
+
+    private
+
+    def message_params
+
+      params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
       
     end
 
 
-    def create
+    def set_group
 
-      @message = @group.messages.new(message_params)
-
-    
-
-      if @message.save
-# binding.pry
-        respond_to do |format|
-          format.html { redirect_to group_messages_path(@group) }
-          format.json
-        end
-
-      else
-        @messages = @group.messages.includes(:user)
-        flash.now[:alert] = 'メッセージを入力してください'
-        render :index
-      end
-
+      @group = Group.find(params[:group_id])
     end
 
 
-  private
 
-  def message_params
-
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-    
   end
-
-
-  def set_group
-
-    @group = Group.find(params[:group_id])
-  end
-
-
-
-end
