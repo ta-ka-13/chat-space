be281a2330983cae25cc78bfe12308e619f72f0b
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-02-28 15:46:53 +0900

    コントローラー_ルート_モデル_ビュー_ja.yml変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :models
   root 'groups#index'
 
-  resources :users, only: [:index,:edit, :update]
-  resources :edit, only: [:group]
-  resources :groups, only: [:edit, :update, :new, :create] do
+  resources :users, only: [:index,:edit, :update, :destroy]
+  resources :edit, only: [:group, :destroy]
+  resources :groups, only: [:edit, :update, :new, :create, :destroy] do
     resources :messages, only: [:index, :create, :destroy]
 
     namespace :api do
