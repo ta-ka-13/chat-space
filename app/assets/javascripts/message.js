bf8848098d72d67d4e11fe305a784fcf8fe84c5b
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-03-04 00:23:54 +0900

    ヘッダーのグループ表示変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,37 +84,7 @@
     }
 
 
-
-// $(function() {
-//   $(document).on("click", ".delete_id", function (e) {
 //     e.preventDefault();
-//     var deleteMessage = confirm('削除してよろしいでしょうか？');
-//     if(deleteMessage == true) {
-//       var message_element = $(this).parents('.content');
-//       var message_id = message_element.attr("data-message-id");
-//       var url = location.href + "/" + message_id;
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: {'id': message_id,
-//       '_method': 'DELETE'} ,
-//       dataType: 'json'
-//     })
-//     .done(function(data) {
-//       message_element.remove();
-//     })
-
-//     .fail(function() {
-//       alert('message destroy error');
-//     })
-//   }
-//   });
-// }); 
-
-
-
-
-
     $('.new_message').on('submit',function(e){
       e.preventDefault()
 
