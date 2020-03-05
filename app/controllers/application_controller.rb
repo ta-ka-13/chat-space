e4222543b39d23466c8c560b547d60cec6dc1abb
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-03-06 00:42:24 +0900

    スマホ画面+smart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  include UserAgent
+  # protect_from_forgery with: :exception
+  before_action :detect_device_variant
   protected
 
   def configure_permitted_parameters
-
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-
   end
-
+  def detect_device_variant
+    request.variant = :smart if is_mobile?
+  end
 
 end
